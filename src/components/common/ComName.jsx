18dabf6835806395a6f85f6797b17cc8f2756f59
Mr.wang <jiucheng0605@gmail.com>
2018-06-24 10:50:17 +0800

    ready for router

---
+++ src/components/common/ComName.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+class ComName extends React.Component {
+    constructor(props) {
+        super(props)
+        this.submitClick = this.submitClick.bind(this)
+    }
+
+    submitClick(event) {
+        
+    }
+    render() {
+        return (
+            <div className="common-name">
+                
+            </div>
+        )
+    }
+}
+
+module.exports = ComName
---
+++ src/components/pages/PageDemo.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+class PageName extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = []
+    }
+}
+
+module.exports = PageName
