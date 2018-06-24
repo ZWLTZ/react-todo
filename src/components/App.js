5e4904c5fce800fa412915e363ab11477a57ee59
Mr.wang <jiucheng0605@gmail.com>
2018-06-24 11:09:03 +0800

    add react-router-dom for react-router

--- src/components/App.js
+++ src/components/App.js
@@ -17,14 +17,20 @@ let Api = Ajax("GET","https://api.github.com/gists").then( res => {
  * 4、redux
  */
 
-// 五、评论
+// 一、测试路由
+import HomeRouter from "./Router.jsx"
+ 
+// 、简单的评论
 import Comments from "./Comments.jsx"
+
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h3 className = "index-title">五、简单的评论</h3>
+				<h3 className = "router-title">一、测试路由</h3>
+				<HomeRouter />
+				<h3 className = "index-title">二、简单的评论</h3>
 				<Comments />
 			</div>
 		);
---
+++ src/components/Router.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const HomeRouter = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                {/* <li><Link to="/topics">Topics</Link></li> */}
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            {/* <Route path="/topics" component={Topics}/> */}
+        </div>
+    </Router>
+  )
+  export default HomeRouter
